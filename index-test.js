47fcf44d5577c09217759b3bd6c6c820e92cdc38
NataUstyuzhanina <mns087@yandex.ru>
2023-05-17 18:05:32 +0500

    test

---
+++ index-test.js
@@ -0,0 +1,25 @@
+console.log('Одеться'); 
+
+console.log('Взять деньги');
+
+console.log('Взять пакет');
+
+console.log('Выйти на улицу');
+
+console.log('Подойти к началу дороги');
+
+console.log('Посмотреть налево');
+
+const isRoadFree = true;
+
+if (isRoadFree) {
+    console.log('Идти прямо');
+
+    // идти дальше по алгоритму
+} else {
+    console.log('Ждать 5 секунд');
+}
+
+
+
+let myAge = 36;
